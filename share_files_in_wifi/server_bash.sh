85848653231967e02d79cd16290cdb1924707738
Mohit Talreja <mohittalrejaceh@gmail.com>
2025-04-06 15:57:55 +0530

    Added QR scan functionality for Share files via WIFI

--- share_files_in_wifi/server_bash.sh
+++ share_files_in_wifi/server_bash.sh
@@ -14,14 +14,65 @@ while [[ $# -gt 0 ]]; do
       DIRECTORY="$2"
       shift 2  # Skip past the -d and its value
       ;;
+    -q)
+      q="$2"  # Correct the shift to get the value of -q
+      shift 2  # Skip past the -q and its value
+      ;;
     *)
-      echo "Usage: $0 [-p <port>] [-d <directory>]"
+      echo "Usage: $0 [-p <port>] [-d <directory>] [-q <generate_qr then provide 'y'>]"
       exit 1
       ;;
   esac
 done
 
+LINK="http://${IPADD}:${PORT}"
 
-echo "Starting Server with Address: ${IPADD}:${PORT} for directory ${DIRECTORY}"
+echo "Starting Server with Address: ${LINK} for directory ${DIRECTORY}"
+
+
+if [ "$q" = 'y' ]; then
+
+  python3 - <<EOF
+try:
+  import qrcode
+  from rich.console import Console
+  from rich.text import Text
+except ImportError:
+  print("Unable to generate QR please install dependencies ... [qrcode, rich]")
+  exit()
+
+CHARACTER = chr(9608)   # █ Character ASCII Value
+
+qr = qrcode.QRCode(
+    version=1, error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=1, border=2,
+)
+
+qr.add_data("${LINK}")
+qr.make(fit=True)
+qr_image = qr.get_matrix()
+qr_string = ""
+
+console = Console()
+
+for row in qr_image:
+    for col in row:
+        if col:
+            qr_string += CHARACTER
+        else:
+            qr_string += " "
+    qr_string += "\n"
+
+qr_text = Text(qr_string, style="bold red on white")
+console.print(qr_text)
+
+EOF
+
+fi
+
+echo -e "\033[31;4mStarted Server at ${LINK}\033[0m"
+# echo -e "\Started Server at ${LINK}"
 python3 -m http.server $PORT -d $DIRECTORY
+
+
 echo "Killing Server...."
